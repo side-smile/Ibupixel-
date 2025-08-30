6510f97af76039c51cbe9f072ef759eaee3eb7df
side-smile <ibrahimsaiid066@gmail.com>
2025-08-30 17:16:28 +0300

    Update repo.js

--- commander/repo.js
+++ commander/repo.js
@@ -4,7 +4,7 @@ const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "repo", catégorie:"Général", reaction: "😄", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
   const githubRepo = 'https://api.github.com/repos/side-smile/Ibupixel-${repoName';
-  const img = 'https://files.catbox.moe/idioc5.jpg';';
+  const img = 'https://files.catbox.moe/ccz8ht.jpg';
 
   try {
     const response = await fetch(githubRepo);
@@ -21,7 +21,7 @@ zokou({ nomCom: "repo", catégorie:"Général", reaction: "😄", nomFichier: __
       const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
       const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
 
-      const gitdata = ` *hellow whatsaap user this is*  
+      const gitdata = ` *hellow whatsaap user this is 𝑀𝑅 𝐵² - 𝑀𝐷*  
 *𝑀𝑅 𝐵² - 𝑀𝐷.*\n support our channel *by*,  https://whatsapp.com/channel/0029VbAjawl9MF8vQQa0ZT32
 
 _________● *𝑀𝑅 𝐵² - 𝑀𝐷* ●____________
@@ -32,8 +32,8 @@ _________● *𝑀𝑅 𝐵² - 𝑀𝐷* ●____________
 |🕐 *ᴜᴘᴅᴀᴛᴇ ᴏɴ:* ${repoInfo.lastUpdate}
 |👨‍💻 *ᴏᴡɴᴇʀ:* *𝑀𝑅 𝐵² - 𝑀𝐷*
 |💞 *ᴛʜᴇᴍᴇ:* *𝑀𝑅 𝐵² - 𝑀𝐷*
-|🥰 *ᴏɴʟʏ ɢᴏᴅ ᴄᴀɴ ᴊᴜᴅɢᴇ ᴍᴇ!👑*
-_________________________________
+|🥰*ᴏɴʟʏ ɢᴏᴅ ᴄᴀɴ ᴊᴜᴅɢᴇ ᴍᴇ!👑*
+__________________________________
             *ᴍᴀᴅᴇ ᴡɪᴛʜ 𝑀𝑅 𝐵² - 𝑀𝐷*`;
 
       await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
